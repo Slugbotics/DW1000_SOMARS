b418034ad30bfa233a3cd21c3bb711084a22d586
Alex <awaghili@ucsc.edu>
2023-05-19 18:09:58 -0700

    Short change at line 29-33 that allows for getting (and printing) the Unique ID of the device in the required format to start the tag as a unique ID

--- examples/DW1000Ranging_TAG/DW1000Ranging_TAG.ino
+++ examples/DW1000Ranging_TAG/DW1000Ranging_TAG.ino
@@ -26,7 +26,12 @@ void setup() {
   //DW1000Ranging.useRangeFilter(true);
   
   //we start the module as a tag
-  DW1000Ranging.startAsTag("7D:00:22:EA:82:60:3B:9C", DW1000.MODE_LONGDATA_RANGE_ACCURACY);
+  char uniqueID[UNIQUE_ID_LENGTH];
+  DW1000.getPrintableExtendedUniqueIdentifier(uniqueID);
+  uniqueID[UNIQUE_ID_LENGTH - 1] = '\0';
+  printf("%s", uniqueID);
+  DW1000Ranging.startAsTag(uniqueID, DW1000.MODE_LONGDATA_RANGE_ACCURACY);
+}
 }
 
 void loop() {
